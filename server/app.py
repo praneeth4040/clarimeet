fd34e6678220751bfff46bb81933358abc205583
praneeth <praneethchakka23@gmail.com>
2025-01-30 03:45:46 +0530

    added summarizer for url

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from components.audioInput import record_audio
 from components.speech_to_text import speech_to_text
 from geminiAi import send_ai_res
+from components.geminiApiForurl import get_summary
 import os
 
 app = FastAPI()
@@ -22,5 +23,10 @@ def transcript_audio_endpoint():
 @app.get("/send_res")
 def send_generated_res(question: str,transcript:str):
     completed_script = send_ai_res(question , transcript)
-    
+
     return completed_script
+
+@app.get("/url_summary")
+def send_summary(url:str):
+    response = get_summary(url)
+    return response
